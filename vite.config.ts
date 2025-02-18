2d3d07f6512dc9b5d8d614a6d0f247742558e60f
Aryan Pandey <134038788+ar-yan-pandey@users.noreply.github.com>
2025-02-18 12:21:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,20 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    rollupOptions: {
+      // Add this to resolve routing issues
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
+  // Add this to support client-side routing
+  server: {
+    historyApiFallback: true
+  }
 });
